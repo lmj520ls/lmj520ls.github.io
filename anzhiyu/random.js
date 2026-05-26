a232963570b0a745212259f42062abcf307ca877
lmj520ls <lmingjun510@gmail.com>
2026-05-26 11:12:18 +0800

    Site updated: 2026-05-26 11:12:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/17/1/"];function toRandomPost(){
+var posts=["2026/05/17/1/","2026/05/26/自由曲面光学基本概念与设计流程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
